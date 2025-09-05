d9d934e9bb5b62e422dbcb5b17bcb2e286789dd0
Valentino <valebtinbest@gmail.com>
2025-09-05 15:05:14 +0200

    init

--- src/DB/managers/users_manager.py
+++ src/DB/managers/users_manager.py
@@ -1,17 +1,17 @@
 from DB.managers.base_manager import BaseAlchemyManager
 from sqlalchemy.dialects.postgresql import insert
 
-from DB.models.users import AlchemyAssets
+from DB.models import AlchemyUsers
 
 
 class UserManager(BaseAlchemyManager):
     """Manager class for user operations."""
     async def create_user_if_not_exist(self,user_id:int,username:str):
         stmt = (
-            insert(AlchemyAssets)
+            insert(AlchemyUsers)
             .values(telegram_id=user_id, username=username)
-            .on_conflict_do_nothing( index_elements=[AlchemyAssets.telegram_id, AlchemyAssets.username])
-            .returning(AlchemyAssets)
+            .on_conflict_do_nothing(index_elements=[AlchemyUsers.telegram_id])
+            .returning(AlchemyUsers)
         )
         result = await self.session.execute(stmt)
         user = result.scalar_one_or_none()
